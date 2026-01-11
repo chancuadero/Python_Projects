c594bc7442507535109e7f595e39882784911a9f
Christian Cuadero <christiancuadero@gmail.com>
2026-01-12 00:53:24 +0800

    Update main.py

--- Slot Machine/main.py
+++ Slot Machine/main.py
@@ -1,4 +1,26 @@
+import random
+
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_sping(rows,cols,symbols):
+    all_symbols = []
+    for symbol,symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    
 
 def deposit():
     while True:
@@ -24,11 +46,33 @@ def get_number_of_lines():
                 print("Enter a valid number of lines.")
         else:
             print("Please enter a number.")
+    return lines
+
+def get_bet():
+    while True:
+        amount = input("How much would you like to bet? $")
+        if amount.isdigit(): 
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}")
+        else:
+            print("Please enter a number.")
+    return amount
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance)
-    print(lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You don't have enough to bet that amount, your current balance is: ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
 
 main()
