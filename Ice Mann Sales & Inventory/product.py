5bf346e36b6396c148039f7af406177e24007f6b
Christian Cuadero <90768537+chancuadero@users.noreply.github.com>
2026-03-13 10:52:25 +0800

    Update product.py
    Added another function called process_sale.

--- Ice Mann Sales & Inventory/product.py
+++ Ice Mann Sales & Inventory/product.py
@@ -5,16 +5,15 @@ class Product:
         self.current_stock = current_stock
     
     def __str__(self):
-        return f"{self.name} (${self.price})"
+        return f"Name: {self.name}, Price: ${self.price}, Stock: {self.current_stock}"
 
-small_bag = Product("1kg ice cubes", 100, 20)
-print(f"Name: {small_bag.name}")
-print(f"Price: {small_bag.price}")
-print(f"Current Stocks: {small_bag.current_stock}")
+small_bag = Product("1kg ice cubes", 10, 20)
+print(small_bag)
 
 class IceTracker:
     def __init__(self):
         self.inventory = {}
+        self.revenue = 0
 
     def add_product(self, product):
         self.inventory[product.name] = product
@@ -23,7 +22,23 @@ class IceTracker:
         for product_object in self.inventory.values():
             print(f"Product: {product_object.name} | Stock: {product_object.current_stock}")
 
+    def process_sale(self, quantity, name):
+        if name in self.inventory.keys():
+            product = self.inventory[name]
+            if name == product.name:
+                if quantity <= product.current_stock:
+                    product.current_stock -= quantity
+                    self.revenue = quantity * product.price
+                    print(f"Sold {quantity} of {name}")
+                    print(f"Revenue: ${self.revenue}")
+                else:
+                    print("Not enough ice cubes!")
+        else:
+            print("Incorrect product name! Sample: 1kg ice cubes")
+
+
 my_tracker = IceTracker()
 my_tracker.add_product(small_bag)
-print(my_tracker.inventory["1kg ice cubes"].price)
+my_tracker.display_inventory()
+my_tracker.process_sale(1, "1kg ice cubes")
 my_tracker.display_inventory()
